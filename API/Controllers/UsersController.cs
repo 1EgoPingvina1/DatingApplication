c4a3202af845eae6bd03e1d3306193f99af08444
Andrew <sinkov407@gmail.com>
2023-08-08 06:50:10 +0300

    Add shared module

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
     public class UsersController : BaseAPIController
     {
         private readonly DataContext _context;
