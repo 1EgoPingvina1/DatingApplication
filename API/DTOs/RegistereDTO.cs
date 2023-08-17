ad9fa07360c46d46e8d2b4066e1d16892230cd23
Andrew <sinkov407@gmail.com>
2023-08-17 20:25:06 +0300

    End with validation data

--- API/DTOs/RegistereDTO.cs
+++ API/DTOs/RegistereDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace API.DTOs
 {
@@ -6,7 +7,13 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
-        
+
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
+
         [Required]
         [StringLength(20,MinimumLength = 8)]
         public string Password { get; set; }
