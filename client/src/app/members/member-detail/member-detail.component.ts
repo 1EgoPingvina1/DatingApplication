0991e5b660b1cb36a58ff3d15c380c4a961cc4a3
Andrew <sinkov407@gmail.com>
2023-08-23 02:10:36 +0300

    End with filters

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GalleryItem, GalleryModule, ImageItem,GalleryComponent } from 'ng-gallery';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TimeagoModule } from 'ngx-timeago';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -11,7 +12,7 @@ import { MembersService } from 'src/app/_services/members.service';
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
   standalone: true,
-  imports: [CommonModule,TabsModule,GalleryModule]
+  imports: [CommonModule,TabsModule,GalleryModule, TimeagoModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
@@ -19,7 +20,6 @@ export class MemberDetailComponent implements OnInit {
 
   constructor(private memberService:MembersService,private route: ActivatedRoute)
   {
-
   }
 
   ngOnInit(): void {
